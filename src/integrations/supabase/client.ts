1cdca19a21bd8976517808ed6841fb482d523468
SAOUADOGO Wendyam Enock Epiphane <enocksaouadogo@gmail.com>
2026-08-11 21:33:39 +0000

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -63,6 +63,7 @@ function createSupabaseClient() {
       storage: typeof window !== "undefined" ? localStorage : undefined,
       persistSession: true,
       autoRefreshToken: true,
+      flowType: 'pkce',
     },
   });
 }
@@ -77,9 +78,3 @@ export const supabase = new Proxy({} as ReturnType<typeof createSupabaseClient>,
     return Reflect.get(_supabase, prop, receiver);
   },
 });
-auth: {
-  storage: typeof window !== "undefined" ? localStorage : undefined,
-  persistSession: true,
-  autoRefreshToken: true,
-  flowType: 'pkce',
-},
